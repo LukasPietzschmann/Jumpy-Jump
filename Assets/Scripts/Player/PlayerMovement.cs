8d37e70f0f2224c5fe747bb95e6efba68d01d259
Lukas Pietzschmann <lukas.pietzschmann@outlook.de>
2021-05-20 15:22:15 +0200

    Added Scripts

---
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+	[SerializeField] private PlayerController controller;
+	[SerializeField] private float runSpeed = 40f;
+	[SerializeField] private Animator animator;
+
+	private float horizontalMove = 0f;
+	private bool jump = false;
+
+	void Update()
+	{
+		horizontalMove = Input.GetAxisRaw("Horizontal") * runSpeed;
+		if (Input.GetButtonDown("Jump"))
+			jump = true;
+		
+		animator.SetFloat("Speed", Mathf.Abs(horizontalMove));
+		animator.SetBool("Jump", jump);
+	}
+
+	private void FixedUpdate()
+	{
+		controller.Move(horizontalMove * Time.fixedDeltaTime, jump);
+		jump = false;
+	}
+}
